41cb29b436b6a6ee13e19c178aa6fc569d0291ac
T P <student5@daniellpedia.com>
2020-10-25 16:56:17 +0000

    sketchy auth stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useContext } from "react";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import ProfilePage from "./ProfilePage";
+import PasswordReset from "./PasswordReset";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { UserContext } from "./providers/UserProvider";
 function App() {
+  const user = useContext(UserContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        user ?
+        <ProfilePage />
+      :
+        <BrowserRouter>
+          <Switch>
+              <Route exact path='/' component={SignIn} />
+              <Route path='/signUp' component={SignUp} />
+              <Route path="/passwordReset" component={PasswordReset} />
+          </Switch>
+        </BrowserRouter>
+
   );
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAM6np9mIhYkRpkg4jRQKUAfDtv6WA65Qc",
+  authDomain: "kanban-16a8e.firebaseapp.com",
+  databaseURL: "https://kanban-16a8e.firebaseio.com",
+  projectId: "kanban-16a8e",
+  storageBucket: "kanban-16a8e.appspot.com",
+  messagingSenderId: "462961531842",
+  appId: "1:462961531842:web:1ebeaba4ae124b5b78945b",
+  measurementId: "G-LD6XE7BZJ9"
+};
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export function signOut = firebase.auth().signOut
